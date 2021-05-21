8159f63f9ff39557281794fa71c29330c318609d
SilvioPazienza <68232994+SilvioPazienza@users.noreply.github.com>
2021-05-21 10:55:26 +0200

    new activity and modify gradle files

---
+++ app/src/main/java/com/manuelmacaj/bottomnavigation/View/loginPackage/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.manuelmacaj.bottomnavigation.View.loginPackage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.manuelmacaj.bottomnavigation.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
