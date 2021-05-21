7a835c827c7f958e1667e71f00c91ccdd6653d35
SilvioPazienza <68232994+SilvioPazienza@users.noreply.github.com>
2021-05-21 12:19:22 +0200

    new activity and layout for register a user

--- app/src/main/java/com/manuelmacaj/bottomnavigation/View/loginPackage/LoginActivity.kt
+++ app/src/main/java/com/manuelmacaj/bottomnavigation/View/loginPackage/LoginActivity.kt
@@ -1,12 +1,79 @@
 package com.manuelmacaj.bottomnavigation.View.loginPackage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.manuelmacaj.bottomnavigation.R
+import kotlinx.android.synthetic.main.activity_login.*
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
+
+    private val mAuth = FirebaseAuth.getInstance() //istanza firebase, sezione autenticazione
+    private val TAG = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //verifico se l'utente ha fatto la login o meno
+
+    }
+
+    fun openRegisterActivity(v: View?){ //funzione consente di aprire activity per la registrazione
+        val intent = Intent(this@LoginActivity, RegisterActivity::class.java) //primo paramatro->context;secondo parametro->activity che deve essere eseguita
+        startActivity(intent)
+    }
+
+    fun checkLogin(v: View?) { //deve capire se quello inserito in username e password è valido o meno e nel caso segnalare
+        val email: String = editTextEmailLogin.getText().toString()
+        if(!isValidEmail(email)){
+            editTextEmailLogin.setError(getResources().getString(R.string.invalid_email))
+            return
+        }
+
+        val pwd = editTextPasswordRegister.getText().toString()
+        if(!isValidPassword(pwd)){
+            editTextPasswordRegister.setError(getResources().getString(R.string.invalid_password)) //...settiamo un errore
+            return //non proseguo (guard)
+        }
+        ChecktoFirebase(email, pwd)
+    }
+
+    private fun ChecktoFirebase(email: String, password: String){ //verifichiamo se utente è autenticato o meno
+        //verifichiamo email e password utente
+        mAuth.signInWithEmailAndPassword(email, password)
+            //verifico ciò che è successo
+            .addOnCompleteListener(this){ task ->
+                if (task.isSuccessful) { //tutto ok
+                    Toast.makeText(this, "Accesso effettuato", Toast.LENGTH_LONG).show()
+                }
+                else { //autenticazione non è andata a buon fine
+                    Toast.makeText(this, "Accesso negato, registrati", Toast.LENGTH_LONG).show()
+                    Log.w(TAG, "Accesso fallito", task.exception)
+                }
+            }
+    }
+
+    private fun isValidPassword(pwd: String): Boolean {
+        return if ((pwd != null) && (pwd.length >= 4))
+            true
+        else false
+    }
+
+    // validating email id
+    private fun isValidEmail(email: String): Boolean {
+        val EMAIL_PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches() //se indirizzo email è soddisfatto, tutto bene sennò...
     }
 }
---
+++ app/src/main/java/com/manuelmacaj/bottomnavigation/View/loginPackage/RegisterActivity.kt
@@ -0,0 +1,112 @@
+package com.manuelmacaj.bottomnavigation.View.loginPackage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.manuelmacaj.bottomnavigation.R
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.regex.Pattern
+
+class RegisterActivity : AppCompatActivity() {
+
+    private var TAG = ".RegisterActivity"
+    private lateinit var nameSurname: EditText
+    private lateinit var emailField: EditText
+    private lateinit var firstPasswordField: EditText
+    private lateinit var confirmPasswordField: EditText
+
+    private val mRegister = FirebaseAuth.getInstance() //istanza firebase riferita alla sezione di autenticazione
+    //istanza firestore riferita alla collezione utenti. Se non esiste, la crea
+    private val mFireStore = FirebaseFirestore.getInstance().collection("Utenti")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        nameSurname = findViewById(R.id.editTextPersonNameSurname)
+        emailField = findViewById(R.id.editTextEmailRegister)
+        firstPasswordField = findViewById(R.id.editTextPasswordRegister)
+        confirmPasswordField = findViewById(R.id.editTextConfirmPassword)
+    }
+
+    fun checkRegister(v: View?) { //deve capire se quello inserito in username e password è valido o meno e nel caso segnalare
+
+        val nomeCognome = nameSurname.text.toString()
+        val email = emailField.text.toString()
+        val password = firstPasswordField.text.toString()
+        val confermaPassword = confirmPasswordField.text.toString()
+
+        if(nomeCognome.isEmpty()) {
+            //settiamo un errore se il campo in cui inserire nome e cognome è vuoto
+            editTextPersonNameSurname.error = resources.getString(R.string.empty_name_surname)
+            return
+        }
+
+        if(!isValidEmail(email)){
+            //settiamo un errore se l'email inserita dall'utente non è valida
+            editTextEmailRegister.error = resources.getString(R.string.invalid_email)
+            return
+        }
+
+        if(!isValidPassword(password)){
+            //settiamo un errore se la password inserita dall'utente non soddisfa i requisiti di lunghezza
+            editTextPasswordRegister.error = resources.getString(R.string.invalid_password)
+            return
+        }
+
+        if (!isValidPassword(confermaPassword) && (confermaPassword != password)){
+            //settiamo un errore se le password non combaciano tra loro
+            editTextConfirmPassword.error = resources.getString((R.string.password_check))
+            return
+        }
+
+        registerNewAccount(nomeCognome, email, password)
+    }
+
+    private fun registerNewAccount(nomeCognome: String, email: String, password: String) {
+        mRegister.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {task -> //proviamo a registrare utente
+            if(task.isSuccessful){ //email utente non era presente nella sezione di autenticazione
+                val userAuth = mRegister.currentUser //prelevo informazioni utente
+                val id = userAuth!!.uid
+                val userMap = HashMap<String, Any>()
+                userMap["ID utente"] = id
+                userMap["Nome e Cognome"] = nomeCognome
+                userMap["Email"] = email
+                //creazione documento
+                mFireStore.document(id).set(userMap).addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){ //creazione documento andata a buon fine
+                        Toast.makeText(this, "Registrazione completata", Toast.LENGTH_LONG).show()
+                        finish()
+                    }
+                    else { //caricamento dati su firestore non riuscito
+                        Toast.makeText(this, "Registrazione fallita", Toast.LENGTH_LONG).show()
+                        Log.w(TAG, "Accesso fallito", task.exception)
+                    }
+                }
+            }
+            else {
+                Log.w(TAG, "Email già esistente", task.exception)
+            }
+        }
+    }
+
+    private fun isValidPassword(pwd: String): Boolean {
+        return if ((!pwd.isEmpty()) && (pwd.length >= 4))
+            true
+        else false
+    }
+
+    // validating email id
+    private fun isValidEmail(email: String): Boolean {
+        val EMAIL_PATTERN = ("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        val matcher = pattern.matcher(email)
+        return matcher.matches() //se indirizzo email è soddisfatto, tutto bene sennò...
+    }
+}
