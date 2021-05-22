a55966668c0148a881e31f9829bd27d164ed5807
SilvioPazienza <68232994+SilvioPazienza@users.noreply.github.com>
2021-05-22 11:49:38 +0200

    improving constructor

--- app/src/main/java/com/manuelmacaj/bottomnavigation/Model/Utente.kt
+++ app/src/main/java/com/manuelmacaj/bottomnavigation/Model/Utente.kt
@@ -1,36 +1,8 @@
 package com.manuelmacaj.bottomnavigation.Model
 
- class Utente { //costruttore della classe utente
+class Utente(var idUtente: String, var nomeCognomeUtente: String, var emailUtente: String) { //costruttore della classe utente
 
-    //dichiarazione variabili con annessi i metodi getters e setters
-     var idUtente: String = "default value"
-
-     private var nomeCognomeUtente: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
-     private var emailUtente: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
-     private var passwordUser: String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-
-    constructor(idUtente: String, nomeCognomeUtente: String, emailUtente: String) { //costruttore della classe Utente
-        this.idUtente = idUtente
-        this.nomeCognomeUtente = nomeCognomeUtente
-        this.emailUtente = emailUtente
-
-    }
-
-    fun toStringUtente(): String { //metodo toString
+    fun toStringUtente(): String { //metodo toStringUtente
         return "ID utente: ${idUtente}, nome e cognome: ${nomeCognomeUtente}, email utente: ${emailUtente}"
     }
 }
